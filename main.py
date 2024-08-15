6f4b8c3744545ef9e91b9bf331ecbb3f12a14776
Xavier <collantes.xavier@gmail.com>
2024-08-15 00:19:15 -0700

    Add fallback
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 
 import logging
 import os
+import random
+from uuid import uuid4
 
 from dotenv import load_dotenv
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
+from telegram.ext import (
+    ApplicationBuilder,
+    filters,
+    CommandHandler,
+    ContextTypes,
+    InlineQueryHandler,
+    MessageHandler,
+)
 
 load_dotenv()
 
@@ -23,8 +32,13 @@ def main() -> None:
 
     # Command on the Telegram app is `/start`
     start_handler: CommandHandler = CommandHandler("start", start)
+    # Inline is `@my_bot some query`
+    caps_handler: InlineQueryHandler = InlineQueryHandler(inline_caps)
+    fallback_handler: MessageHandler = MessageHandler(filters.COMMAND, fallback)
 
     telegram_app.add_handler(start_handler)
+    telegram_app.add_handler(caps_handler)
+    telegram_app.add_handler(fallback_handler)  # Error fallback handlers must be last.
 
     # Start running Telegram app indefinitely and receive commands.
     telegram_app.run_polling()
@@ -35,7 +49,7 @@ def build_application(token: str) -> ApplicationBuilder:
     return ApplicationBuilder().token(token).build()
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Receives a Telegram message that contains the /start command.
 
     Args:
@@ -48,6 +62,39 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=f"Hello {update.effective_sender.name}",
+    )
+
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Handler for inline.
+
+    If you want to implement inline functionality for your bot, please first
+    talk to @BotFather and enable inline mode using /setinline. It sometimes
+    takes a while until your Bot registers as an inline bot on your client.
+    """
+    query: str = update.inline_query.query
+    if not query:
+        return
+
+    results: list = []
+    results.append(
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            title="All caps",
+            input_message_content=InputTextMessageContent(query.upper()),
+        )
+    )
+
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+async def fallback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Catch all when command is not valid such as `/commandnotexist`."""
+    responses: list[str] = ["Uh say again?", "Not a command.", "I didn't understand."]
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=random.choice(responses),
     )
 
 
